82d18520c541de16c478a0c20689edd7f340f448
wisaTong <wisa.po@ku.th>
2018-07-21 21:52:56 +0700

    idk

--- server/routes.js
+++ server/routes.js
@@ -3,5 +3,6 @@ const line = require('./lineController')
 module.exports.initialize = (app) => {
   
   app.post('/api/alert', line.sendMessage)
+  app.get('/api/test', line.test)
 
 }
--- services/db.js
+++ services/db.js
@@ -1,12 +1,6 @@
-const mongoose = require('mongoose');
-
 let HOST_URL = 'localhost'
 if (process.env.NODE_ENV==='production') HOST_URL = 'mongo'
 const PORT = process.env.DB_PORT || 27018
 
 mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb://${HOST_URL}:${PORT}/Grader_SKE`);
-
-module.exports = {
-  mg_: mongoose,
-};
